36f00991eee3bb876b2a503d7b41076563269967
DoHyunHwang <dohyun033@gmail.com>
2021-07-10 19:20:22 +0900

    add CardAddForm component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
@@ -7,6 +7,41 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Dohyun',
+      company: 'Null',
+      theme: 'dark',
+      title: '프론트엔드 개발자',
+      email: 'dohyun033@gmail.com',
+      message: 'Just do it',
+      fileName: 'dohyun',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'Dohyun2',
+      company: 'Null',
+      theme: 'light',
+      title: '프론트엔드 개발자',
+      email: 'dohyun033@gmail.com',
+      message: 'Just do it',
+      fileName: 'dohyun',
+      fileURL: null,
+    },
+    {
+      id: '3',
+      name: 'Dohyun3',
+      company: 'Null',
+      theme: 'colorful',
+      title: '프론트엔드 개발자',
+      email: 'dohyun033@gmail.com',
+      message: 'Just do it',
+      fileName: 'dohyun',
+      fileURL: null,
+    }
+  ]);
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -19,12 +54,17 @@ const Maker = ({ authService }) => {
       }
     });
   });
+
+  const addCard = (card) => {
+    const updated = [...cards, card];
+    setCards(updated);
+  }
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} addCard={addCard}/>
+        <Preview cards={cards}/>
       </div>
       <Footer />
     </section>
